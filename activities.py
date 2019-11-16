b4b7188b1387519756e5797fc861a471deafb7b1
Denise Ratasich <dratasich@users.noreply.github.com>
2019-11-16 18:30:28 +0100

    Remove office draft (no permissions for app registration)

--- activities.py
+++ activities.py
@@ -7,7 +7,7 @@ import subprocess
 
 
 # start date
-datefrom = datetime.date.today().replace(day=1)
+date_from = datetime.date.today().replace(day=1)
 
 # arguments
 desc = "List activities per date."
@@ -15,10 +15,8 @@ parser = argparse.ArgumentParser(description=desc)
 parser.add_argument('-g', '--git', metavar="DIRECTORY", type=str,
                     help="""Directory including git repos to list commits
                     from.""")
-parser.add_argument('-o', '--office365', metavar="USERNAME:PASSWORD", type=str,
-                    help="""Credentials to extract O365 calendar entries.""")
-parser.add_argument('-f', '--from', default=datefrom,
-                    help=f"""Start date. Defaults to {datefrom} (first day of
+parser.add_argument('-f', '--from', default=date_from,
+                    help=f"""Start date. Defaults to {date_from} (first day of
                     the current month).""")
 args = parser.parse_args()
 
@@ -40,12 +38,8 @@ if __name__ == '__main__':
             # get author name from git repo
             author = execute(f'cd {d}; git config user.name').strip()
             # print one line commits
-            out += execute(f"""cd {d}; git log --date=iso --since={datefrom} --pretty=format:'%ad: %s' --author='{author}'""") + "\n"
+            out += execute(f"""cd {d}; git log --date=iso --since={date_from} --pretty=format:'%ad: %s' --author='{author}'""") + "\n"
         lines.extend(out.split('\n'))
-
-    # o365 calendar events
-    if args.office365 is not None:
-        print("o365")
 
     # cleanup
     lines = list(map(lambda l: l.strip(), lines))
