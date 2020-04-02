753047b72ec91ec4efcd8288bdc3910ce530af06
Ratasich Denise (BCC.IKT) <Denise.Ratasich@oebb.at>
2020-04-02 19:57:32 +0200

    Add analysis of the development on the story point per sprint

---
+++ story-points-analysis/analyze.py
@@ -0,0 +1,63 @@
+# To add a new cell, type '# %%'
+# To add a new markdown cell, type '# %% [markdown]'
+
+# %%
+import pandas as pd
+
+
+# %%
+data = pd.read_csv("2020-04-01_finished-stories-per-sprint.csv", sep=';')
+data.head()
+
+
+# %%
+# Number of sprints
+data["Sprint"].nunique()
+
+
+# %%
+# Distribution of Story Points
+data.hist("Story Points")
+
+
+# %%
+data["Story Points"].value_counts()
+
+
+# %%
+# filter columns of interest without NaN values
+points_per_sprint = data[["Story Points", "Sprint"]].dropna()
+# remove "Sprint "
+points_per_sprint["Sprint"] = points_per_sprint["Sprint"].apply(lambda s: int(str(s).replace("Sprint", "").strip()))
+# fibonacci to linear story points
+unique_points = sorted(data["Story Points"].unique())
+points_per_sprint["Points (linear)"] = points_per_sprint["Story Points"].apply(lambda p: unique_points.index(p)+1)
+# no points at all
+points_per_sprint["Count"] = 1
+points_per_sprint.head()
+
+
+# %%
+# Sum of points per sprint
+points_per_sprint.groupby("Sprint").sum().plot()
+
+
+# %%
+# Number of stories per sprint
+points_per_sprint.groupby("Sprint")["Count"].sum().plot()
+
+
+# %%
+# Stories had more points at the beginning
+col = "Story Points"
+for col in ["Story Points", "Points (linear)"]:
+    points_per_sprint.groupby("Sprint").agg(
+        min=(col, min),
+        max=(col, max),
+        mean=(col, lambda x: x.mean())
+    ).plot()
+
+
+# %%
+
+
