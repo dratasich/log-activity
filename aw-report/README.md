0b126b9581230e041d06c391951d45d59ca294f5
Denise Ratasich <dratasich@users.noreply.github.com>
2020-12-27 16:28:30 +0100

    Query and print start, end and not-afk duration

--- aw-report/README.md
+++ aw-report/README.md
@@ -3,7 +3,7 @@ Report
 
 Reads events from buckets per day and prints:
 
-- [ ] start and end of first and last afk-event (login and shutdown)
-- [ ] active time (sum of not-afk durations)
+- [x] start and end of first and last afk-event (login and shutdown)
+- [x] active time (sum of not-afk durations)
 - [ ] git issues and commits (aw-git-hooks events)
 - [ ] time spent in meetings (window events containing "MS Teams")
--- aw-report/report.py
+++ aw-report/report.py
@@ -1,23 +1,54 @@
 #!/usr/bin/env python3
 
 # %% Imports
+from aw_core.models import Event
 from aw_client import ActivityWatchClient
-from datetime import datetime
+from datetime import datetime, timedelta
 
 
 # %% Settings
-BUCKET_AFK="aw-watcher-afk_nils"
+BUCKET_AFK = "aw-watcher-afk_nils"
+DATE_FROM = datetime.today().replace(day=1, hour=4, minute=0, second=0, microsecond=0)
+DATE_TO = datetime.now()
 
-# %% Init
-client = ActivityWatchClient("report-client")
 
 # %% Helpers
-def toDate(date: str):
-    return datetime.fromisoformat(date)
 
-# %% Get all events from afk bucket
-query = f"RETURN = query_bucket('{BUCKET_AFK}');"
-res = client.query(query, [(toDate("2020-12-26"), toDate("2020-12-27"))])
+def str_date(date):
+    return date.strftime('%Y-%m-%d')
 
-# %%
-client.disconnect()
+def str_time(date):
+    if date is None:
+        return "00:00"
+    return date.strftime('%H:%M')
+
+
+# %% Get events
+client = ActivityWatchClient("report-client")
+
+date = DATE_FROM
+while date < DATE_TO:
+    query = f"""
+events = query_bucket('{BUCKET_AFK}');
+RETURN = sort_by_timestamp(events);
+"""
+    afk = client.query(query, [(date, date + timedelta(days=1))])
+    date = date + timedelta(days=1)
+
+    # new week formatting
+    weekday = date.weekday()
+    if weekday == 1:
+        print("{:-^80}".format(f" Week {date.isocalendar()[1]} "))
+
+    if len(afk[0]) == 0:
+        # no events at all on this day
+        continue
+
+    # start and end of day
+    first_event = Event(**afk[0][0])
+    last_event = Event(**afk[0][-1])
+    active = timedelta(seconds=sum([e['duration'] for e in afk[0] if e['data']['status'] == "not-afk"]))
+    print(f"{str_date(first_event.timestamp)} {first_event.timestamp.strftime('%a')}"
+          + f" | {str_time(first_event.timestamp)} - {str_time(last_event.timestamp)}"
+          + f" (not-afk {active})"
+          )
