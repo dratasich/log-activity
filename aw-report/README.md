33a641daa57b126182dee6613c0a100188d1694c
Denise Ratasich <dratasich@users.noreply.github.com>
2020-12-28 11:29:02 +0100

    Print git commits by issue

--- aw-report/README.md
+++ aw-report/README.md
@@ -7,3 +7,14 @@ Reads events from buckets per day and prints:
 - [x] active time (sum of not-afk durations)
 - [x] git issues and commits (aw-git-hooks events)
 - [ ] time spent in meetings (window events containing "MS Teams")
+
+
+Usage
+-----
+
+Print report by issue:
+```bash
+$ ./report.py -c issue
+```
+
+See `./report.py -h` for more options.
--- aw-report/report.py
+++ aw-report/report.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python3
 
 # %% Imports
+import argparse
 from aw_core.models import Event
 from aw_client import ActivityWatchClient
 from dataclasses import dataclass, field
 from datetime import datetime, timedelta
+import os.path
+import pandas as pd
 from typing import List
 
 
@@ -13,6 +16,14 @@ BUCKET_AFK = "aw-watcher-afk_nils"
 BUCKET_GIT = "aw-git-hooks_nils"
 DATE_FROM = datetime.today().replace(day=1, hour=4, minute=0, second=0, microsecond=0)
 DATE_TO = datetime.now()
+
+# arguments
+desc = "List activities per date."
+parser = argparse.ArgumentParser(description=desc)
+parser.add_argument('-c', '--commits-sort-by', choices=['issue', 'timestamp'],
+                    default='timestamp',
+                    help=f"""Print commits either per issue or print all commits ordered by time.""")
+args = parser.parse_args()
 
 
 # %% Helpers
@@ -26,12 +37,52 @@ def str_time(date):
     return date.strftime('%H:%M')
 
 @dataclass
-class GitCommit:
+class GitHook:
     hook: str
-    origin: str = "unknown origin"
-    branch: str = "unknown branch"
-    summary: str = "no summary"
+    origin: str = None
+    branch: str = None
+    summary: str = None
     issues: List[str] = field(default_factory=list)
+
+    def __str__(self):
+        issue_list = f" ({', '.join(self.issues)})" if len(self.issues) > 0 else ""
+        return f"  - {self.origin} ({self.branch}): {self.summary}{issue_list}"
+
+@dataclass
+class GitHookDto:
+    hook: str
+    origin: str = None
+    branch: str = None
+    summary: str = None
+    issue: str = None
+
+
+def to_dataframe(hooks: List[GitHook]):
+    # flatten issue list
+    hooksDto = []
+    for h in hooks:
+        new_dict = h.__dict__.copy()
+        issues = new_dict.pop('issues')
+        # no issues referenced
+        if len(issues) == 0:
+            hooksDto.append(GitHookDto(**new_dict))
+            continue
+        # add a row for each issue
+        for i in issues:
+            new_dict['issue'] = i
+            hooksDto.append(GitHookDto(**new_dict))
+    return pd.DataFrame([h.__dict__ for h in hooksDto])
+
+def issue_to_string(i: pd.DataFrame):
+    issue = i.iloc[0].issue
+    summary = ""
+    titles = i[['origin', 'summary']].mask(i.summary.eq('None')).dropna()
+    if len(titles) > 0:
+        repos = []
+        titles.groupby('origin') \
+            .apply(lambda o: repos.append(os.path.basename(o.iloc[0].origin).split('.')[0] + ': ' + ', '.join(o.summary)))
+        summary = f" ({'; '.join(repos)})"
+    return f"{issue if issue != 'None' else 'other'}{summary}"
 
 
 # %% Get events
@@ -71,5 +122,13 @@ RETURN = sort_by_timestamp(events);
 
     # git commits
     if len(git[0]) > 0:
-        commits = [GitCommit(**e['data']) for e in git[0] if e['data']['hook'] == "post-commit"]
-        [print(f"  - {c.origin}: {c.summary} ({', '.join(c.issues)})") for c in commits]
+        hooks = [GitHook(**e['data']) for e in git[0]]
+        if args.commits_sort_by == 'timestamp':
+            [print(h) for h in hooks if h.hook == "post-commit"]
+        elif args.commits_sort_by == 'issue':
+            df = to_dataframe(hooks)
+            commits = df[df.hook == "post-commit"] \
+                .astype(str) \
+                .drop_duplicates() \
+                .groupby('issue') \
+                .apply(lambda g: print(f"  {issue_to_string(g)}"))
