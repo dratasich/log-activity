3d83d3461f28920f514a700645d7df4032746bb7
Denise Ratasich <dratasich@users.noreply.github.com>
2020-12-29 11:22:56 +0100

    Code formatting with black
    - spacemacs love :) it just works using <Tab> :)

--- aw-report/report.py
+++ aw-report/report.py
@@ -2,14 +2,14 @@
 
 # %% Imports
 import argparse
-from aw_core.models import Event
-from aw_client import ActivityWatchClient
+import os.path
 from dataclasses import dataclass, field
 from datetime import datetime, timedelta
-import os.path
-import pandas as pd
 from typing import List
 
+import pandas as pd
+from aw_client import ActivityWatchClient
+from aw_core.models import Event
 
 # %% Settings
 BUCKET_AFK = "aw-watcher-afk_nils"
@@ -20,21 +20,28 @@ DATE_TO = datetime.now()
 # arguments
 desc = "List activities per date."
 parser = argparse.ArgumentParser(description=desc)
-parser.add_argument('-c', '--commits-sort-by', choices=['issue', 'timestamp'],
-                    default='timestamp',
-                    help=f"""Print commits either per issue or print all commits ordered by time.""")
+parser.add_argument(
+    "-c",
+    "--commits-sort-by",
+    choices=["issue", "timestamp"],
+    default="timestamp",
+    help=f"""Print commits either per issue or print all commits ordered by time.""",
+)
 args = parser.parse_args()
 
 
 # %% Helpers
 
+
 def str_date(date):
-    return date.astimezone().strftime('%Y-%m-%d')
+    return date.astimezone().strftime("%Y-%m-%d")
+
 
 def str_time(date):
     if date is None:
         return "00:00"
-    return date.astimezone().strftime('%H:%M')
+    return date.astimezone().strftime("%H:%M")
+
 
 @dataclass
 class GitHook:
@@ -47,6 +54,7 @@ class GitHook:
     def __str__(self):
         issue_list = f" ({', '.join(self.issues)})" if len(self.issues) > 0 else ""
         return f"  - {self.origin} ({self.branch}): {self.summary}{issue_list}"
+
 
 @dataclass
 class GitHookDto:
@@ -62,25 +70,31 @@ def to_dataframe(hooks: List[GitHook]):
     hooksDto = []
     for h in hooks:
         new_dict = h.__dict__.copy()
-        issues = new_dict.pop('issues')
+        issues = new_dict.pop("issues")
         # no issues referenced
         if len(issues) == 0:
             hooksDto.append(GitHookDto(**new_dict))
             continue
         # add a row for each issue
         for i in issues:
-            new_dict['issue'] = i
+            new_dict["issue"] = i
             hooksDto.append(GitHookDto(**new_dict))
     return pd.DataFrame([h.__dict__ for h in hooksDto])
+
 
 def issue_to_string(i: pd.DataFrame):
     issue = i.iloc[0].issue
     summary = ""
-    titles = i[['origin', 'summary']].mask(i.summary.eq('None')).dropna()
+    titles = i[["origin", "summary"]].mask(i.summary.eq("None")).dropna()
     if len(titles) > 0:
         repos = []
-        titles.groupby('origin') \
-            .apply(lambda o: repos.append(os.path.basename(o.iloc[0].origin).split('.')[0] + ': ' + ', '.join(o.summary)))
+        titles.groupby("origin").apply(
+            lambda o: repos.append(
+                os.path.basename(o.iloc[0].origin).split(".")[0]
+                + ": "
+                + ", ".join(o.summary)
+            )
+        )
         summary = f" ({'; '.join(repos)})"
     return f"{issue if issue != 'None' else 'other'}{summary}"
 
@@ -91,13 +105,13 @@ client = ActivityWatchClient("report-client")
 date = DATE_FROM
 while date < DATE_TO:
     query = f"""
-events = query_bucket('{BUCKET_AFK}');
-RETURN = sort_by_timestamp(events);
-"""
+    events = query_bucket('{BUCKET_AFK}');
+    RETURN = sort_by_timestamp(events);
+    """
     afk = client.query(query, [(date, date + timedelta(days=1))])
     query = f"""
-        events = query_bucket('{BUCKET_GIT}');
-        RETURN = sort_by_timestamp(events);
+    events = query_bucket('{BUCKET_GIT}');
+    RETURN = sort_by_timestamp(events);
     """
     git = client.query(query, [(date, date + timedelta(days=1))])
     date = date + timedelta(days=1)
@@ -114,21 +128,26 @@ RETURN = sort_by_timestamp(events);
     # start and end of day
     first_event = Event(**afk[0][0])
     last_event = Event(**afk[0][-1])
-    active = timedelta(seconds=sum([e['duration'] for e in afk[0] if e['data']['status'] == "not-afk"]))
-    print(f"{str_date(first_event.timestamp)} {first_event.timestamp.strftime('%a')}"
-          + f" | {str_time(first_event.timestamp)} - {str_time(last_event.timestamp + last_event.duration)}"
-          + f" (not-afk {active})"
-          )
+    active = timedelta(
+        seconds=sum([e["duration"] for e in afk[0] if e["data"]["status"] == "not-afk"])
+    )
+    print(
+        f"{str_date(first_event.timestamp)} {first_event.timestamp.strftime('%a')}"
+        + f" | {str_time(first_event.timestamp)} - {str_time(last_event.timestamp + last_event.duration)}"
+        + f" (not-afk {active})"
+    )
 
     # git commits
     if len(git[0]) > 0:
-        hooks = [GitHook(**e['data']) for e in git[0]]
-        if args.commits_sort_by == 'timestamp':
+        hooks = [GitHook(**e["data"]) for e in git[0]]
+        if args.commits_sort_by == "timestamp":
             [print(h) for h in hooks if h.hook == "post-commit"]
-        elif args.commits_sort_by == 'issue':
+        elif args.commits_sort_by == "issue":
             df = to_dataframe(hooks)
-            commits = df[df.hook == "post-commit"] \
-                .astype(str) \
-                .drop_duplicates() \
-                .groupby('issue') \
+            commits = (
+                df[df.hook == "post-commit"]
+                .astype(str)
+                .drop_duplicates()
+                .groupby("issue")
                 .apply(lambda g: print(f"  {issue_to_string(g)}"))
+            )
