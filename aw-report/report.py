25eef9a3d5b05a1912c72c1ce5ca683189357f18
Denise Ratasich <dratasich@users.noreply.github.com>
2021-01-16 19:45:31 +0100

    Round start and end of day

--- aw-report/report.py
+++ aw-report/report.py
@@ -61,6 +61,25 @@ def str_time(date):
     return date.astimezone().strftime("%H:%M")
 
 
+def round_timedelta(tm: timedelta, round_to_s=timedelta(minutes=15).total_seconds()):
+    tm_rounded = timedelta(
+        seconds=int((tm.total_seconds() + round_to_s / 2) / (round_to_s)) * (round_to_s)
+    )
+    logging.debug(f"{round_to_s}s-rounded {tm} to {tm_rounded}")
+    return tm_rounded
+
+
+def round_datetime(tm: datetime, round_to_min=15):
+    tm_rounded = tm + timedelta(minutes=float(round_to_min) / 2)
+    tm_rounded -= timedelta(
+        minutes=tm_rounded.minute % round_to_min,
+        seconds=tm_rounded.second,
+        microseconds=tm_rounded.microsecond,
+    )
+    logging.debug(f"{round_to_min}s-rounded {tm} to {tm_rounded}")
+    return tm_rounded
+
+
 @dataclass
 class GitHook:
     hook: str
@@ -143,9 +162,7 @@ while date < DATE_TO:
         # no events at all on this day
         continue
 
-    # start and end of day
-    first_event = Event(**afk[0][0])
-    last_event = Event(**afk[0][-1])
+    # active time
     active = timedelta(
         seconds=sum([e["duration"] for e in afk[0] if e["data"]["status"] == "not-afk"])
     )
@@ -166,17 +183,17 @@ while date < DATE_TO:
     if active_incl_short_pauses >= timedelta(hours=6):
         logging.debug(f"add 30min break")
         working_hours += timedelta(minutes=30)
-    round_s = timedelta(minutes=15).total_seconds()
-    working_hours_rounded = timedelta(
-        seconds=int((working_hours.total_seconds() + round_s / 2) / (round_s))
-        * (round_s)
-    )
-    logging.debug(
-        f"working hours: {working_hours}, {round_s}s-rounded to {working_hours_rounded}"
-    )
+    working_hours_rounded = round_timedelta(working_hours)
+
+    # start and end of day
+    first_event = Event(**afk[0][0])
+    last_event = Event(**afk[0][-1])
+    start = round_datetime(first_event.timestamp)
+    end = round_datetime(last_event.timestamp + last_event.duration)
+
     print(
-        f"{str_date(first_event.timestamp)} {first_event.timestamp.strftime('%a')}"
-        + f" | {str_time(first_event.timestamp)} - {str_time(last_event.timestamp + last_event.duration)}"
+        f"{str_date(start)} {start.strftime('%a')}"
+        + f" | {str_time(start)} - {str_time(end)}"
         + f" ({working_hours_rounded})"
     )
 
