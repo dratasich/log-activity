aa9e06a3ac185b28ad76198ba9925aac2bfbe475
Denise Ratasich <dratasich@users.noreply.github.com>
2021-01-20 18:21:53 +0100

    Refactor GitHook models

---
+++ aw-report/models.py
@@ -0,0 +1,43 @@
+from dataclasses import dataclass, field
+from typing import List, Optional
+
+import pandas as pd
+
+
+@dataclass
+class GitHook:
+    hook: str
+    origin: Optional[str] = None
+    branch: Optional[str] = None
+    summary: Optional[str] = None
+    issues: List[str] = field(default_factory=list)
+
+    def __str__(self):
+        issue_list = f" ({', '.join(self.issues)})" if len(self.issues) > 0 else ""
+        return f"  - {self.origin} ({self.branch}): {self.summary}{issue_list}"
+
+
+@dataclass
+class GitHookDto:
+    hook: str
+    origin: Optional[str] = None
+    branch: Optional[str] = None
+    summary: Optional[str] = None
+    issue: Optional[str] = None
+
+
+def to_dataframe(hooks: List[GitHook]):
+    # flatten issue list
+    hooksDto = []
+    for h in hooks:
+        new_dict = h.__dict__.copy()
+        issues = new_dict.pop("issues")
+        # no issues referenced
+        if len(issues) == 0:
+            hooksDto.append(GitHookDto(**new_dict))
+            continue
+        # add a row for each issue
+        for i in issues:
+            new_dict["issue"] = i
+            hooksDto.append(GitHookDto(**new_dict))
+    return pd.DataFrame([h.__dict__ for h in hooksDto])
--- aw-report/report.py
+++ aw-report/report.py
@@ -5,13 +5,14 @@ import argparse
 import logging
 import os.path
 import socket
-from dataclasses import dataclass, field
 from datetime import datetime, timedelta
 from typing import List, Optional
 
 import pandas as pd
 from aw_client import ActivityWatchClient
 from aw_core.models import Event
+
+from models import *
 
 # %% Settings
 BUCKET_AFK = f"aw-watcher-afk_{socket.gethostname()}"
@@ -80,45 +81,6 @@ def round_datetime(tm: datetime, round_to_min=15):
     )
     logging.debug(f"{round_to_min}s-rounded {tm} to {tm_rounded}")
     return tm_rounded
-
-
-@dataclass
-class GitHook:
-    hook: str
-    origin: Optional[str] = None
-    branch: Optional[str] = None
-    summary: Optional[str] = None
-    issues: List[str] = field(default_factory=list)
-
-    def __str__(self):
-        issue_list = f" ({', '.join(self.issues)})" if len(self.issues) > 0 else ""
-        return f"  - {self.origin} ({self.branch}): {self.summary}{issue_list}"
-
-
-@dataclass
-class GitHookDto:
-    hook: str
-    origin: Optional[str] = None
-    branch: Optional[str] = None
-    summary: Optional[str] = None
-    issue: Optional[str] = None
-
-
-def to_dataframe(hooks: List[GitHook]):
-    # flatten issue list
-    hooksDto = []
-    for h in hooks:
-        new_dict = h.__dict__.copy()
-        issues = new_dict.pop("issues")
-        # no issues referenced
-        if len(issues) == 0:
-            hooksDto.append(GitHookDto(**new_dict))
-            continue
-        # add a row for each issue
-        for i in issues:
-            new_dict["issue"] = i
-            hooksDto.append(GitHookDto(**new_dict))
-    return pd.DataFrame([h.__dict__ for h in hooksDto])
 
 
 def issue_to_string(i: pd.DataFrame):
